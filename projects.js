a3edd76c98801aa434903d5369bb0d3a67d25e48
thulka2 <thulka2@uic.edu>
2021-04-07 20:17:05 -0500

    started projects list

---
+++ projects.js
@@ -0,0 +1,50 @@
+/* 
+project name
+brief description
+technologies/languages used
+link to repo
+link to functioning page
+image (a screen grab of the project would be good)
+display (a boolean indicating whether or not to show this item.)
+
+
+*/
+
+
+let projects = [
+
+    {
+        "project_name" : "Valorskins",
+        "description" : "A personal project showcasing the skins of the popular competitive game 'VALORANT'. ",
+        "technologies" : "JS, HTML, CSS, React, React-MDL",
+        "repo_link" : "https://github.com/thulka2/valorskins",
+        "page_link" : "https://valorskins.com/",
+        "image" : "",
+        "display" : true
+
+    },
+    {
+        "project_name" : "Scrolling Things",
+        "description" : "A scrolling game themed around the 'Stranger Things' show. Difficulty scales as game progresses.",
+        "technologies" : "JS, HTML, CSS, PWA",
+        "repo_link" : "https://github.com/thulka2/IT202-Spring2021-project2",
+        "page_link" : "https://thulka2.github.io/IT202-Spring2021-project2/",
+        "image" : "",
+        "display" : true
+
+    },
+    {
+        "project_name" : "Chicago Land Inventory",
+        "description" : "A single-page App that allows users to filter and map Chicago City-Owned Land Inventory.",
+        "technologies" : "JS, HTML, CSS, Maps API",
+        "repo_link" : "https://github.com/thulka2/IT202-Spring2021-project1",
+        "page_link" : "https://thulka2.github.io/IT202-Spring2021-project1/",
+        "image" : "",
+        "display" : true
+
+    },
+    {}
+
+
+
+]
